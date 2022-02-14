1f7911f0de5ef104799995f0fd0db125f28ccf62
Geri <luga.geri@gmail.com>
2022-02-14 15:24:33 +0100

    Created component for single quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { fetchQuotes } from "./api";
+import { fetchQuotes, fetchRandomQuote } from "./api";
+import SingleQuote from "./Components/SingleQuote";
 import { IQuote } from "./types";
 
 function App() {
   const [quotes, setQuotes] = useState<IQuote[]>([]);
+  const [randomQuote, setRandomQuote] = useState<IQuote | null>(null);
 
   useEffect(() => {
     fetchQuotes().then((serverQuotes) => setQuotes(serverQuotes));
@@ -12,13 +14,19 @@ function App() {
   return (
     <>
       <ul className="quote-list">
-        {quotes.map((quote) => (
-          <li>
-            <h3>bla bl bla bla bla bla bla</h3>
-            <h4>- Geri Luga</h4>
-          </li>
+        {quotes.map(({ id, text, author }) => (
+          <SingleQuote key={id} text={text} author={author} />
         ))}
       </ul>
+      <h2>Random Quote</h2>
+      <button
+        onClick={(e) =>
+          fetchRandomQuote().then((quote) => setRandomQuote(quote))
+        }
+      >
+        Get random quote
+      </button>
+      {randomQuote && <SingleQuote text={randomQuote.text} author={randomQuote.author} />}
     </>
   );
 }
